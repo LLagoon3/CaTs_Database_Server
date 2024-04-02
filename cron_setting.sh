94c2b114b44b9d5dbe610ee61f0a4aaca213c2d0
LLagoon3 <choonarm3@gmail.com>
2024-04-02 16:31:07 +0900

    chore: MOD cron_setting.sh

--- cron_setting.sh
+++ cron_setting.sh
@@ -3,21 +3,18 @@
 OS=$(uname -s)
 
 if [ "$OS" == "Linux" ]; then
-    DISTRO=$(lsb_release -is)
-    if [ "$DISTRO" == "Ubuntu" ]; then
-        echo "Ubuntu"
-        apt-get install -y cron
-    elif [ "$DISTRO" == "Darwin" ]; then
-        echo "Mac OS"
-        brew install cron 
-    else
-        echo "Not Linux"
-    fi
+    echo "Linux"
+    sudo apt-get install -y cron
+elif [ "$DISTRO" == "Darwin" ]; then
+    echo "Mac OS"
+    brew install cron 
 else
-    echo "OS Not Found"
+    echo "Not Linux"
 fi
+
 
 mkdir /tmp/mysql
 SCRIPT_DIR=$(pwd)
 { crontab -l && echo "* * * * * $SCRIPT_DIR/mysql_backup.sh > /tmp/mysql/mysql_backup.log 2>&1"; } | crontab -
-
+sudo usermod -aG docker $USER
+docker exec mysql-server GRANT PROCESS ON *.* TO 'cats'@'localhost'
